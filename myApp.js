fe62369ccc1aeb59ec412c0e83a23a5a09535215
Nuel <ibk.com@gmail.com>
2022-03-30 00:38:22 +0100

    myapp index sending a file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+
+
 console.log("Hello World");
 
 app.get('/', (_, res) => {
-  res.send('Hello Express');
+  // res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
