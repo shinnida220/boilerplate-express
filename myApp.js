8439b16f913547417138edcb9e43f985086d7da1
Nuel <ibk.com@gmail.com>
2022-03-30 00:51:46 +0100

    myapp added static file serving inspection

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 // Serve static files..
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 // Log hellow world to the console
 console.log("Hello World");
