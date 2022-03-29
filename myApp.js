42851b06fe211a179140b247b9485676a84a0d1e
Nuel <ibk.com@gmail.com>
2022-03-30 00:44:06 +0100

    myapp added static file serving

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Serve static files..
+app.use(express.static(__dirname + '/public'));
 
-
+// Log hellow world to the console
 console.log("Hello World");
 
 app.get('/', (_, res) => {
+  // Send a static string response
   // res.send('Hello Express');
+
+  // Respond with a file.
   res.sendFile(__dirname + '/views/index.html');
 });
 
